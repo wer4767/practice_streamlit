54cee1f3846e4a6091197cc48f714be3ddc1ab8c
wer4767 <57377683+wer4767@users.noreply.github.com>
2024-09-11 09:07:45 +0900

    test_화면.py

--- test_화면.py
+++ test_화면.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 from sqlalchemy import create_engine
 from streamlit_option_menu import option_menu
 
